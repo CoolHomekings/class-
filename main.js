4a4f4a0f08b1e6fd3ee56dbf41d7be2256cc4ae1
CoolHomekings <111741014+CoolHomekings@users.noreply.github.com>
2024-01-04 16:21:13 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Img.fromURL("birthdayImage.jpg",function(Img)){
+    block_img_object=Img
+}
+}
+
+function playSound(){
+
+}
